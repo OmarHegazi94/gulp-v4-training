8119340765f0f64ed473def11d3ecac751de8f66
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-02-01 12:16:39 +0200

    added gulp-less + compiled all less files to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,40 @@
 const gulp = require('gulp');
+const path = require('path');
+const less = require('gulp-less');
 
+
+
+// Assets
+
+    // => CSS
+
+    // => JS
+
+    // => Fonts
+
+    // => Img
+
+
+
+// Compile LESS to CSS
+gulp.task('compile-less', async () => {
+    return gulp.src('./style/css/*.less')
+    .pipe( less({
+        paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+
+
+// Compile all ES6 to ES5
+// Minify all css
+// Rename all css
+// Minify all images 
+// See how to Minify fonts
+
+
+// Copy all of these results to new dist folder
+
+
+// See how to make a local dev server and watch for changes
