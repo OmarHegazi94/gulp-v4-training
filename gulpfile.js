b80489dea0c30c75a3a9657c64f2c84cf4c487e8
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-02-01 13:21:21 +0200

    installed + tested babel + concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const path = require('path');
 const less = require('gulp-less');
-
-
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
 // Assets
 
@@ -25,14 +25,32 @@ gulp.task('compile-less', async () => {
     .pipe(gulp.dest('dist/assets/css'));
 });
 
+// Minify all css
 
 
 // Compile all ES6 to ES5
-// Minify all css
+
+// gulp.task('compile-js', async () => {
+//     return gulp.src('./style/js/*.js')
+//     .pipe(babel({
+//         presets: ['@babel/preset-env']
+//     }))
+//     .pipe(gulp.dest('./dist/assets/js/'));
+// });
+
+// Minify all js files
+gulp.task('concat-js', async () => {
+    return gulp.src('./style/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/assets/js/'));
+});
+
+
 // Rename all css
 // Minify all images 
 // See how to Minify fonts
-
+// Copy all html files into dist folder
+// Minify all html files
 
 // Copy all of these results to new dist folder
 
